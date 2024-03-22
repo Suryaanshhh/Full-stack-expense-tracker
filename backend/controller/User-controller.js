6dc179135794830659c5cb59d7e225fdc8cdbc4f
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-03-22 11:28:19 +0530

    password is safe !!!

--- backend/controller/User-controller.js
+++ backend/controller/User-controller.js
@@ -1,39 +1,47 @@
 
 const { response } = require('express');
 const User = require('../model/user');
+const bcrypt = require('bcrypt');
+
 
 exports.register = (req, res, next) => {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-    User.create({
-        name: name,
-        email: email,
-        password: password
-    }).then((data) => {
-        console.log(data)
-        res.status(201).json({ User: data })
-    }).catch(err => console.log(err))
+    bcrypt.hash(password, 10, async (err, hash) => {
+        console.log(err)
+        await User.create({
+            name: name,
+            email: email,
+            password: hash
+        }).then((data) => {
+            console.log(data)
+            res.status(201).json({ User: data })
+        }).catch(err => console.log(err))
+    })
 }
 
 exports.Login = (req, res, next) => {
     const email = req.params.email;
     const password = req.body.password;
-   // console.log(`firts pass is ${password}`);
+    // console.log(`firts pass is ${password}`);
     User.findAll({ where: { email: email } }).then((user) => {
         if (user.length > 0) {
-            if (user[0].password == password) {
-                //console.log(`second pass is ${user[0].password}`);
-                res.status(201).json({ message: "Login Successfull" })
-            }
-            else {
-                res.status(401).json({ message: "Incorrect Password" })
-            }
-        }
-        else {
-            res.status(404).json({ message: "user not found" })
+            bcrypt.compare(password, user[0].password, (err, result) => {
+                if (err) {
+                    res.status(500).json({ success: false, message: 'Something went wrong' });
+                }
+                if (result == true) {
+                    //console.log(`second pass is ${user[0].password}`);
+                    res.status(201).json({ message: "Login Successfull" });
+                } else {
+                    res.status(401).json({ message: "Incorrect Password" });
+                }
+            });
+        } else {
+            res.status(404).json({ message: "user not found" });
         }
     }).catch(err => {
-        console.log(err)
-    })
-}
+        console.log(err);
+    });
+};
