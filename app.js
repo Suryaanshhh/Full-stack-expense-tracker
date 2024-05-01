10793fe8f932faacd9270ea219a14f92fdbc2a2e
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-05-01 14:35:28 +0530

    production Ready

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
 const app = express();
-const sq = require('./backend/util/database')
-const User=require('./backend/model/user')
-const Expense=require('./backend/model/expense')
-const Order=require('./backend/model/orders')
-const Request=require('./backend/model/ForgotPasswordRequests')
-const FileUrl=require('./backend/model/fileUrl')
-const UserRoute = require('./backend/routes/user-routes');
-const premiumRoute=require('./backend/routes/premium-routes');
-const forgetPass=require('./backend/routes/forget-pass-route');
-const expenseRoute=require('./backend/routes/expense-Routes');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+const sq = require("./backend/util/database");
+const User = require("./backend/model/user");
+const Expense = require("./backend/model/expense");
+const Order = require("./backend/model/orders");
+const Request = require("./backend/model/ForgotPasswordRequests");
+const FileUrl = require("./backend/model/fileUrl");
+const UserRoute = require("./backend/routes/user-routes");
+const premiumRoute = require("./backend/routes/premium-routes");
+const forgetPass = require("./backend/routes/forget-pass-route");
+const expenseRoute = require("./backend/routes/expense-Routes");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 app.use(bodyParser.json({ extended: false }));
-app.use(cors());
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  {
+    flags: "a",
+  }
+);
+
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(morgan("combined",{stream:accessLogStream}));
 //Route Configuration
 app.use(UserRoute);
 app.use(forgetPass);
@@ -23,7 +38,7 @@ app.use(premiumRoute);
 
 //schema Relations
 User.hasMany(Expense);
-Expense.belongsTo(User)
+Expense.belongsTo(User);
 User.hasMany(Order);
 Order.belongsTo(User);
 User.hasMany(Request);
@@ -32,6 +47,6 @@ User.hasMany(FileUrl);
 FileUrl.belongsTo(User);
 //schema Relations
 
-sq.sync()
+sq.sync();
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
--- backend/util/database.js
+++ backend/util/database.js
@@ -1,4 +1,10 @@
-const {Sequelize} =require('sequelize');
-const sq=new Sequelize('new-db','root','suryansh',{ dialect:'mysql',host:"localhost"});
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const sq = new Sequelize(
+  `${process.env.DB_SCHEMA}`,
+  `${process.env.DB_USER}`,
+  `${process.env.DB_PASS}`,
+  { dialect: "mysql", host: `${process.env.DB_HOST}` }
+);
 
-module.exports=sq;
+module.exports = sq;
