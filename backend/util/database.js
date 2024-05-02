7a7cc36b00c3aee341ca3a5bfb3aca45984df913
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-05-02 10:25:01 +0530

    some Glitch

--- backend/util/database.js
+++ backend/util/database.js
@@ -4,7 +4,7 @@ const sq = new Sequelize(
   `${process.env.DB_SCHEMA}`,
   `${process.env.DB_USER}`,
   `${process.env.DB_PASS}`,
-  { dialect: "mysql", host: `${process.env.DB_HOST}` }
+  { dialect: "mysql", host: `${process.env.DB_HOST}`  }
 );
 
 module.exports = sq;
