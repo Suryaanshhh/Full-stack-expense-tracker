2c765ba7021d0980c428eba810cbefde797b7524
Suryaanshhh <153890779+Suryaanshhh@users.noreply.github.com>
2024-05-03 23:20:55 +0530

    script tag  fixed

--- frontend/signup/script.js
+++ frontend/signup/script.js
@@ -1,24 +1,24 @@
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-const submitBtn=document.getElementById('submit');
-submitBtn.addEventListener('click',function(event){
-    event.preventDefault();
-    const name=document.getElementById('name').value;
-    const email=document.getElementById('email').value;
-    const password=document.getElementById('password').value;
-
-    const RegisterUser={
-        name:name,
-        email:email,
-        password:password
-    }
-    if(name&&email&&password){
-        axios.post('http://16.170.98.135:3000/register-user',RegisterUser).then((response)=>{
-        console.log(response)
-        window.location.href="../login/login.html"
-    }).catch(err=>console.log(err))
-    }
-    else{
-        alert('Fill all detail')
-    }
-   
-})
+  const RegisterUser = {
+    name: name,
+    email: email,
+    password: password,
+  };
+  if (name && email && password) {
+    axios
+      .post("http://16.170.98.135:3000/register-user", RegisterUser)
+      .then((response) => {
+        console.log(response);
+        window.location.href = "../login/login.html";
+      })
+      .catch((err) => console.log(err));
+  } else {
+    alert("Fill all detail");
+  }
+});
